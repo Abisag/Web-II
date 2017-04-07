617e122c0933f6629b2ae0a1d681025c215f2b8b
Abisag <abisagabigayl@hotmail.com>
2017-04-06 18:45:57 -0600

    agregar archivos

--- pagina/index.php
+++ pagina/index.php
@@ -1,6 +1,6 @@
 <?php
 	require('conexion.php');
-	$query="SELECT * FROM cdr";
+	$query="SELECT id,calldate,src,dst,dcontext,lastapp,duration,disposition FROM cdr_data";
 	$resultado=$mysqli->query($query);
 ?>
 
@@ -10,36 +10,37 @@
 		<title class="titulo">Registro de llamadas</title>
 		
 	</head>
-	<body >
+	<body class="texto2">
 	<form name="asterisk" method="post" accion="index.php">
-		<div id="nombre"><center><h1>Registro de llamadas</h1></center></div>
-		<input type="submit" name="ID" value="ID">
-		<input type="submit" name="extenision" value ="Fuente">
-		<input type="submit" name="fecha" value="Fecha">
-		<SELECT NAME="Combo" SIZE=1> 
-			<OPTION VALUE="CBID">ID</OPTION>
+		<div id="nombre"><center><h1>CENTRO DE LLAMADAS MDA.</h1></center></div>
+		<input class="bottom" type="submit" name="ID" value="ID">
+		<input class="bottom" type="submit" name="fecha" value="Fecha">
+		<input class="bottom" type="submit" name="enviar" value="Buscar">
+		<SELECT class="texto1" NAME="Combo" SIZE=1> 
 			<OPTION VALUE="CBFuente">Fuente</OPTION>
 			<OPTION VALUE="CBDestino">Destino</OPTION> 
 		</SELECT> 
 		<input type="text" name="txt_ext">
-		
 		<p></p>
 		<div id="tabla">
 			<table border=1 width="80%">
 			<thead>
-				<tr>
+				<tr class="texto1">
 					<td class="texto1"><b>ID</b></td>
-					<td><b>Fecha</b></td>
-					<td><b>Fuente</b></td>
-					<td><b>Destino</b></td>
+					<td class="texto1"><b>Fecha</b></td>
+					<td class="texto1"><b>Fuente</b></td>
+					<td class="texto1"><b>Destino</b></td>
 					
 					
-					<td><b>Duracion</b></td>
 				</tr>
 				<tbody>
 					
 					<?php 
 
+					//if(isset($_POST["enviar"])){
+					//	$env=$_POST["txt_ext"];
+					//	$query="SELECT id,calldate,src,dst,dcontext,lastapp,duration,disposition FROM cdr_data where src=$env";
+					//}
 					if(isset($_POST["enviar"])){
 						$env=$_POST["txt_ext"];
 						$op=$_POST["Combo"];
@@ -64,24 +65,16 @@
 					while($row=$resultado->fetch_assoc()){ ?>
 						<tr>
 							
-							<td><?php echo $row['id'];?>
+							<td class="texto3"><?php echo $row['id'];?>
 							</td>
-							<td>
+							<td class="texto3" >
 								<?php echo $row['calldate'];?>
 							</td>
-							<td>
-								<?php echo $row['src'];?>
-							</td>
-							<td>
-								<?php echo $row['dst'];?>
-							</td>
-							<td><?php echo $row['dcontext'];?>
-							</td>
 							
-							<td>
+							<td class="texto3">
 								<?php echo $row['duration'];?>
 							</td>
-							<td>
+							<td class="texto3">
 								<?php if ($row['lastapp']=="VoiceMail") {
 									echo "VOICEMAIL";
 								}
